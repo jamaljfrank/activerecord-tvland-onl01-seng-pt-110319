b6475bf1babd5c0e400083104e46256f9cf4c35d
Jamal Frank <jamaljfrank@gmail.com>
2020-02-04 17:29:54 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
 end
+
+  def self.full_name
+    
+    puts self.first_name 
+  end
+  
+  def list_roles
+  end
+
+
